830531fcf9a99aedcdb0f14a804b4a3298bbebe8
José Cuentas Turpo <jcuentast@gmail.com>
2017-09-06 15:21:11 -0500

    feat: ui testing:  - showing recipes in mainactivity

--- app/src/main/java/com/josecuentas/android_testingeffective/ui/main/MainActivity.java
+++ app/src/main/java/com/josecuentas/android_testingeffective/ui/main/MainActivity.java
@@ -2,13 +2,23 @@ package com.josecuentas.android_testingeffective.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.josecuentas.android_testingeffective.R;
+import com.josecuentas.android_testingeffective.data.local.RecipeStore;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recipes);
+        RecipeStore store = new RecipeStore(this, "recipes");
+        RecipeAdapter adapter = new RecipeAdapter(store);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
