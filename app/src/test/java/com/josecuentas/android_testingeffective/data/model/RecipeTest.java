cb6751f93dc700d2d3c0e3e4bfa58cd67b7c9a3e
José Cuentas Turpo <jcuentast@gmail.com>
2017-09-06 12:12:43 -0500

    feat: test as you go:  - tdd: red, green, refactor, part 2

--- app/src/test/java/com/josecuentas/android_testingeffective/data/model/RecipeTest.java
+++ app/src/test/java/com/josecuentas/android_testingeffective/data/model/RecipeTest.java
@@ -10,9 +10,12 @@ import static org.junit.Assert.*;
 public class RecipeTest {
     @Test
     public void water() {
-        InputStream stream = RecipeTest.class.getResourceAsStream("/recupes/water.txt");
+        InputStream stream = RecipeTest.class.getResourceAsStream("/recipes/water.txt");
         Recipe recipe = Recipe.readFromStream(stream);
         assertNotNull(recipe);
+        assertEquals("water", recipe.id);
+        assertEquals("Water", recipe.title);
+        assertEquals("Put glass under tap. Open tap. Close tap. Drink.", recipe.description);
     }
 
 }
