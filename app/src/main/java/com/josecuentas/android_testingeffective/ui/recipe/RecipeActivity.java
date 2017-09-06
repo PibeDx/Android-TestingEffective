1a30f6ffb6abef523f480f058af75275a91170d6
José Cuentas Turpo <jcuentast@gmail.com>
2017-09-06 15:46:55 -0500

    feat: ui testing:  - recipeactivity loading a recipe

--- app/src/main/java/com/josecuentas/android_testingeffective/ui/recipe/RecipeActivity.java
+++ app/src/main/java/com/josecuentas/android_testingeffective/ui/recipe/RecipeActivity.java
@@ -6,8 +6,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.TextView;
 
 import com.josecuentas.android_testingeffective.R;
+import com.josecuentas.android_testingeffective.data.local.RecipeStore;
+import com.josecuentas.android_testingeffective.data.model.Recipe;
 
 /**
  * Created by jcuentas on 6/09/17.
@@ -20,5 +23,21 @@ public class RecipeActivity extends AppCompatActivity {
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        TextView titleView = (TextView) findViewById(R.id.title);
+        TextView descriptionView = (TextView) findViewById(R.id.description);
+
+        RecipeStore store = new RecipeStore(this, "recipes");
+        String id = getIntent().getStringExtra(KEY_ID);
+        Recipe recipe = store.getRecipe(id);
+
+        if (recipe == null) {
+            titleView.setVisibility(View.GONE);
+            descriptionView.setText(R.string.recipe_not_found);
+            return;
+        }
+
+        titleView.setText(recipe.title);
+        descriptionView.setText(recipe.description);
     }
 }
